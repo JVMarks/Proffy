a66ced12ce65032f927538f1f9fb0543016a193b
JVMarks <jovic.marks@gmail.com>
2020-08-20 13:56:34 -0300

    backend1.0

--- backend/src/controllers/userController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import mongoose from "mongoose";
-
-export default class authenticateContoller {
-  async index(request: Request, response: Response) {
-    const User = mongoose.model("User");
-
-  } 
-}
